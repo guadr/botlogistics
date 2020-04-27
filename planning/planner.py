94fd3636675fc670f8201b44bc5a737bb4bb07c8
Luke Hartman <ljhartman10@gmail.com>
2020-04-27 23:08:28 +0000

    adjust planning.py

--- planning/planner.py
+++ planning/planner.py
@@ -1,8 +1,6 @@
 import queuer
-# TODO: add __init__.py file to botlogistics/tracking and .
 from tracking import fleet
 
 class Planner(object):
     def __init__(self):
-       self.queue = querer.FleetQueue()
-       self.fleet = Fleet.info()
+       raise NotImplementedError()
